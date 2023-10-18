f922be8f9e47d57fe30962fffa6da52d3626a31f
Daniel Campos Martins <danicoxa.martins@outlook.com>
2023-10-18 12:20:37 -0300

    primeiro_ticket

--- src/main/java/com/teste/App.java
+++ src/main/java/com/teste/App.java
@@ -1,4 +1,5 @@
 package com.teste;
+import java.util.*;
 
 /**
  * Hello world!
@@ -8,6 +9,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Conjuntos!" );
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(null);
+        System.out.println(s1);
+        System.out.println("O conjunto s1 contém o número 1? ");
+        System.out.println(s1.contains(1));
+        System.out.println("O conjunto s1 contém o número 6? ");
+        System.out.println(s1.contains(6));
+
+        Set<Integer> empty = new HashSet<>();
+        empty.add(2);
+        empty.add(null);
+        System.out.println("O conjunto s1 contém o conjunto vazio? ");
+        System.out.println(s1.containsAll(empty));
     }
 }
--- src/test/java/com/teste/AppTest.java
+++ src/test/java/com/teste/AppTest.java
@@ -1,8 +1,10 @@
 package com.teste;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import java.util.*;
 
 /**
  * Unit test for simple App.
@@ -13,8 +15,16 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testeS1()
     {
-        assertTrue( true );
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(null);
+
+        String actual = s1.toString();
+        String expected = "[null, 1, 2, 3]"; 
+        assertEquals( expected, actual );
     }
 }
